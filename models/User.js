6dbcf0cf050f07217916c4851f88d831a947258f
Dan G <daniel.w.gray1994@gmail.com>
2023-07-13 20:12:22 -0400

    created a User model without 'friends' key; still figuring out self-reference

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true, trim: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+      },
+    },
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+});
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const db = require("./config/connection");
 
+const { Username } = require("./models");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
