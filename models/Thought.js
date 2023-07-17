a30470fb2541b6233f8e5b47323faf599ee5d14e
Dan G <daniel.w.gray1994@gmail.com>
2023-07-17 06:16:26 -0400

    added reactionSchema to the Thought.js file

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,29 @@
 const { Schema, Types, model } = require("mongoose");
 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => moment(time).format("MMMM Do YYYY, h:mm:ss a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const thoughtSchema = new Schema({
   thoughtText: { type: String, required: true, maxlength: 280, minlength: 1 },
   createdAt: {
