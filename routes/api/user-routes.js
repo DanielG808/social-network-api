7bc321adab132e4069599f77d9709ba0ace1a577
Dan G <daniel.w.gray1994@gmail.com>
2023-07-17 09:16:15 -0400

    created a user routes and set up some routes; they are broken

---
+++ routes/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const {
+  createUser,
+  getUserById,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/user-controller");
+
+// GET and POST request for all users
+router.route("/").get(getAllUsers).post(createUser);
+
+//GET, PUT, and DELETE for getting a single user, updating, and deleting a single user by ID
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
