3ec7a5c0c3dc957bfea46ea20103e0cbd298d1e8
Dan G <daniel.w.gray1994@gmail.com>
2023-07-17 06:09:12 -0400

    added a virtual to the user model to handle the friends count

--- models/User.js
+++ models/User.js
@@ -1,19 +1,32 @@
 const { Schema, Types, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: { type: String, unique: true, required: true, trim: true },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
       },
     },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
-  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
-  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);
