e229475bff9d1a7e853579fd4eeb497aed8d0ac0
Dan G <daniel.w.gray1994@gmail.com>
2023-07-18 06:51:57 -0400

    added an update thought route

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -39,7 +39,10 @@ const thoughtController = {
   // GET request to get one Thought by id
   async getThoughtById(req, res) {
     try {
-      const thought = await Thought.findOne({ _id: req.params.id });
+      const thought = await Thought.findOne({ _id: req.params.id }).populate({
+        path: "reactions",
+        select: "-__v",
+      });
 
       if (!thought) {
         return res.status(404).json({ message: "No Thought with this ID." });
@@ -53,6 +56,25 @@ const thoughtController = {
   },
 
   // PUT request to update one Thought by id
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true, runValidators: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: "No Thought with this ID." });
+      }
+
+      res.status(200).json(thought);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  },
+
   // DELETE request to delete one Thought by id
   // PUT request to add a reaction to a Thought
   // DELETE request to delete a reaction from a Thought
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,12 +4,13 @@ const {
   createThought,
   getThoughts,
   getThoughtById,
+  updateThought,
 } = require("../../controllers/thought-controller");
 
 // GET and POST request for all thoughts
 router.route("/").get(getThoughts).post(createThought);
 
-// GET all ids
-router.route("/:id").get(getThoughtById);
+// GET PUT and DELETE requests for finding, updating, and deleting a thought based on it's id
+router.route("/:id").get(getThoughtById).put(updateThought);
 
 module.exports = router;
